f8d81005cf3bda266e844fd91f764f34a09be870
Valentinws <valentin.rbarbu@gmail.com>
2022-10-06 06:08:35 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots } from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(searchRobots);
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store} >
+        <App />
+    </Provider>)
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
